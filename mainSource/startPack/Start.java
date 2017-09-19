64ffa2ac82df64a12b6082be983e3e87d40e470a
ValeryYordanov <v.iordanov@paip3.com>
2017-09-19 18:01:37 +0300

    First Commit - The Start

---
+++ mainSource/startPack/Start.java
@@ -0,0 +1,19 @@
+package startPack;
+
+import java.util.HashMap;
+
+import users.User;
+
+public class Start {
+	
+	private HashMap<String, User> allUsers;
+
+	public static User register(String username, String password, String email) {
+		return null;
+	}
+	
+	public static User login(String username, String password) {
+		return null;
+	}
+	
+}
